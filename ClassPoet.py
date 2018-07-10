c3354bc6486f0681e412cebecae68055b07362aa
JZ-W <jane.zhang--@homtmail.com>
2018-07-10 10:38:34 +0800

    define poet class

--- ClassPoet.py
+++ ClassPoet.py
@@ -1,1 +1,19 @@
+class Poet:
+    def __init__(self,name,birth,end,Zi,Hao,Chenghu,hometown):
+        self.name = name
+        self.birth = birth
+        self.end = end
+        self.Zi = Zi
+        self.Hao = Hao
+        self.Chenghu = Chenghu
+        self.hometown = hometown
+
+    def detail(self):
+        print('name: ',self.name)
+        print('birth: ',self.birth)
+        print('end: ',self.end)
+        print('Zi: ',self.Zi)
+        print('Hao: ',self.Hao)
+        print('Chenghu: ',self.Chenghu)
+        print('hometown: ',self.hometown)
 
---
+++ FillInPoets.py
@@ -0,0 +1,3 @@
+from ClassPoet import Poet
+LiBai = Poet('LiBai',761,820,'Taibai','Qinglian',['shixian','Qijue'],'Sichuan')
+LiBai.detail()
